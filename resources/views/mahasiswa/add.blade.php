1f3cf20a342ceda5ad6558308ed3bba8657573c0
richandcapable <mhmmdnabilm@gmail.com>
2023-05-08 10:08:37 +0800

    Menambahkan File

---
+++ resources/views/mahasiswa/add.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+    <h3>Tambah Data Mahasiswa</h3>
+    <form action="{{ url('/mahasiswa') }}" method="post">
+        @csrf
+        <table>
+            <tr>
+                <td>NIM</td>
+                <td><input type="text" name="mhsw_nim"></td>
+            </tr>
+            <tr>
+                <td>NAMA</td>
+                <td><input type="text" name="mhsw_nama"></td>
+            </tr>
+            <tr>
+                <td>JURUSAN</td>
+                <td><input type="text" name="mhsw_jurusan"></td>
+            </tr>
+            <tr>
+                <td>ALAMAT</td>
+                <td><input type="text" name="mhsw_alamat"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="SIMPAN"></td>
+            </tr>
+        </table>
+    </form>
+</div>
+@endsection
